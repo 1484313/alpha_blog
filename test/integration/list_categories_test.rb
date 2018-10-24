b1ea8127b2269bf934074971ede95ff172589798
Kamil <1484313@gmail.com>
2018-10-24 08:42:29 +0200

    admin restricted categories

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -7,9 +7,10 @@ class ListCategoriesTest < ActionDispatch::IntegrationTest
 	end
 
 	test "should show categories listing" do
-		get_categories_path
+		get categories_path
 		assert_template 'categories/index'
-		assert_select ""
+		assert_select "a[href=?]", category_path(@category), text: @category.name
+		assert_select "a[href=?]", category_path(@category2), text: @category2.name
 	end
 
 end
